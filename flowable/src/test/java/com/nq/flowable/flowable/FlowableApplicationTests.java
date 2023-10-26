677f189da0b2dfc32a81b3ce478f13181e44d9e1
Alfred.Ning <15667269283@163.com>
2023-10-26 20:34:46 +0800

    add: 测试流程

--- flowable/src/test/java/com/nq/flowable/flowable/FlowableApplicationTests.java
+++ flowable/src/test/java/com/nq/flowable/flowable/FlowableApplicationTests.java
@@ -1,13 +1,19 @@
 package com.nq.flowable.flowable;
 
+import javax.annotation.Resource;
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngines;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlowableApplicationTests {
+  @Resource
+  private ProcessEngine processEngine;
 
   @Test
   void contextLoads() {
+    System.out.println(processEngine);
   }
 
 }

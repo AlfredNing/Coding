6a2e195643110e767fa67f9415159ff6067d6028
Alfred.Ning <15667269283@163.com>
2023-10-30 23:06:37 +0800

    add: 手动流程部署

--- flowable/src/test/java/com/nq/flowable/flowable/FlowableApplicationTests.java
+++ flowable/src/test/java/com/nq/flowable/flowable/FlowableApplicationTests.java
@@ -3,6 +3,8 @@ package com.nq.flowable.flowable;
 import javax.annotation.Resource;
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngines;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.repository.Deployment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,9 +13,29 @@ class FlowableApplicationTests {
   @Resource
   private ProcessEngine processEngine;
 
+  @Resource
+  private RepositoryService repositoryService;
+
+
   @Test
   void contextLoads() {
     System.out.println(processEngine);
+  }
+
+
+  /**
+   * 手动部署
+   */
+  @Test
+  void testHandle(){
+    Deployment deploy = repositoryService.createDeployment()
+        .addClasspathResource("请假流程.bpmn20.xml")
+        .name("请假流程手动部署")
+        .deploy();
+
+    System.out.println(deploy.getId());
+    System.out.println(deploy.getName());
+
   }
 
 }

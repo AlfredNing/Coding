e1367a3c0a31495e4e61aad23ad18ed74f7773aa
Alfred.Ning <15667269283@163.com>
2023-10-30 23:21:21 +0800

    add: 完成流程

--- flowable/src/test/java/com/nq/flowable/flowable/FlowableApplicationTests.java
+++ flowable/src/test/java/com/nq/flowable/flowable/FlowableApplicationTests.java
@@ -7,13 +7,16 @@ import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngines;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
 import org.flowable.engine.repository.Deployment;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlowableApplicationTests {
+
   @Resource
   private ProcessEngine processEngine;
 
@@ -22,6 +25,9 @@ class FlowableApplicationTests {
 
   @Resource
   private RuntimeService runtimeService;
+
+  @Resource
+  private TaskService taskService;
 
 
   @Test
@@ -34,7 +40,7 @@ class FlowableApplicationTests {
    * 手动部署
    */
   @Test
-  void testHandle(){
+  void testHandle() {
     Deployment deploy = repositoryService.createDeployment()
         .addClasspathResource("请假流程.bpmn20.xml")
         .name("请假流程手动部署")
@@ -47,10 +53,24 @@ class FlowableApplicationTests {
 
   @DisplayName("启动流程")
   @Test
-  public void testStartProcess(){
+  public void testStartProcess() {
     Map<String, Object> map = new HashMap<>();
-    map.put("assignee0","zhangsan");
-    map.put("assignee1","lisi");
-    runtimeService.startProcessInstanceById("请假流程:2:c22d438e-7461-11ee-8524-0871908e2604",map);
+    map.put("assignee0", "zhangsan");
+    map.put("assignee1", "lisi");
+    runtimeService.startProcessInstanceById("请假流程:2:c22d438e-7461-11ee-8524-0871908e2604", map);
+  }
+
+  @DisplayName("完成流程")
+  @Test
+  public void testCompleteTask() {
+    Task task = taskService.createTaskQuery()
+        .processDefinitionId("请假流程:2:c22d438e-7461-11ee-8524-0871908e2604")
+        .taskAssignee("zhangsan")
+        .singleResult();
+
+    if (task != null) {
+      taskService.complete(task.getId());
+      System.out.println("complete");
+    }
   }
 }

8aea6b81777e51174cc990e3e13bee9b9a3720b6
Alfred.Ning <15667269283@163.com>
2024-02-25 13:44:10 +0800

    add:spring data jpa

---
+++ spring-data/data-jpa/src/main/java/com/nq/ddd/AggregateRoot.java
@@ -0,0 +1,8 @@
+package com.nq.ddd;
+
+/**
+ * @author Alfred.Ning
+ * @since 2024年02月25日 13:16:00
+ */
+public interface AggregateRoot<T> extends Entity<T> {
+}
---
+++ spring-data/data-jpa/src/main/java/com/nq/ddd/Entity.java
@@ -0,0 +1,9 @@
+package com.nq.ddd;
+
+/**
+ * @author Alfred.Ning
+ * @since 2024年02月25日 13:11:00
+ */
+public interface Entity<T> {
+
+}

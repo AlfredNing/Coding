f4f2b413ac5d5d427b17b69bbb08f33d6f4818c6
AlfredNing <ning_q@foxmail.com>
2025-08-11 21:15:01 +0800

    初始化项目工程

---
+++ python_ai_full_learn/chat_open_ai/tiktoken.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import tiktoken
+
+load_dotenv()
+
+client = OpenAI()
+
+completion = client.embeddings.create(
+    model="text-embedding-v4",
+    input='衣服的质量杠杠的，很漂亮，不枉我等了这么久啊，喜欢，以后还来这里买',
+    dimensions=1024, # 指定向量维度（仅 text-embedding-v3及 text-embedding-v4支持该参数）
+    encoding_format="float"
+)
+print(completion.model_dump_json())
